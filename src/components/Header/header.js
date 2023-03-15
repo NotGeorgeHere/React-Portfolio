58f0be24a4260a5da1f7a0d92457b25c643f2032
unknown <ryder.george20@gmail.com>
2023-03-15 14:00:17 +0000

    added navbar

---
+++ src/components/Header/header.js
@@ -0,0 +1,45 @@
+import { NavLink, Link} from 'react-router-dom';
+
+function Header(){
+    return(
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link className="navbar-brand" to='/'>React Portfolio</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+
+        <li className="nav-item">
+            <NavLink 
+            to='/' 
+            end 
+            className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+              }>Home Page</NavLink>
+        </li>
+
+        <li className="nav-item">
+            <NavLink 
+            to='/projects' 
+            end 
+            className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+              }>Projects</NavLink>
+        </li>
+
+        <li className="nav-item">
+            <NavLink 
+            to='/contact' 
+            end 
+            className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+              }>Contact Me</NavLink>
+        </li>
+        </ul>
+    </div>
+    </nav>  
+    );
+}
+
+export default Header;
---
+++ src/pages/Contact.js
@@ -0,0 +1,5 @@
+function Contact(){
+    return <div>contacts</div>;
+}
+
+export default Contact;
