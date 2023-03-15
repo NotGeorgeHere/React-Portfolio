5d8733dbc4c6b0319ccfb07adeb100e9298183df
unknown <ryder.george20@gmail.com>
2023-03-15 23:04:14 +0000

    finished homepage and sorted file order

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header/header';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contacts/Contact';
 
 function App() {
   return (
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import './Home.css';
-import avatar from '../images/img_avatar.png';
+import avatar from '../../images/img_avatar.png';
 
 function Home(){
     return (
@@ -11,11 +11,23 @@ function Home(){
                     <img src={avatar} alt="Portfolio avatar" className='avatar'></img>
                 </div>
             </div>
-            <div className='textSection'>
+            <div className='aboutSection'>
                 <h3 className='AboutMe'>About Me</h3>
                 <p className='aboutMeText'>Sed vel viverra ex, a mattis felis. Fusce faucibus diam ut mauris iaculis, ac efficitur turpis sollicitudin. Nullam sit amet massa et felis dapibus facilisis vel non metus. Fusce risus velit, luctus at suscipit et, scelerisque in augue. Donec fermentum turpis quam, sit amet accumsan metus accumsan in. Praesent ultricies magna nec est pretium lacinia sit amet eget nisi. Etiam id lacus ante. Suspendisse sed fringilla quam. Sed suscipit efficitur lacinia. In sagittis velit ut neque dapibus mollis. Sed elementum dui vel risus placerat consectetur. Aliquam bibendum scelerisque porta. Vivamus pretium augue vel sapien efficitur, eu suscipit velit laoreet.</p>
 
                 <p className='aboutMeText'>Nunc a justo sit amet magna varius maximus non sed nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla porttitor, ligula ut fringilla volutpat, eros nibh mattis neque, commodo viverra tellus risus in est. Quisque non feugiat augue. Maecenas a sodales dui. Donec non erat laoreet, eleifend felis et, consectetur eros. Sed et metus lacinia, aliquam mi at, bibendum diam. Nullam vitae pellentesque turpis. Proin mollis, nunc vel pellentesque dictum, mauris lectus vehicula lacus, sit amet viverra quam sapien elementum enim.</p>
+            </div>
+
+            <div className="skillsSection">
+                <h3 className="skills">Skills</h3>
+                <ul className='skillsText'>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu hendrerit urna. </li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu hendrerit urna. Donec commodo luctus vulputate.</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu hendrerit urna. Donec commodo luctus vulputate. Etiam blandit mauris urna, ut cursus turpis ultrices non. </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>    
+                </ul>
             </div>
         </div>      
     );
