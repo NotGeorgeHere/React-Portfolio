8008d60863bda66e7bdd3310d3c246757f8120be
unknown <ryder.george20@gmail.com>
2023-03-18 15:36:25 +0000

    added project card

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,12 +2,13 @@ import "./ProjectCard.css"
 
 function ProjectCard(object){
     return(
-    <div className="card cardStyle">
+    <div className="card cardStyle text-center">
+        <h5 className="card-title">{object.projectName}</h5>
+        <hr></hr>
         <img src={object.screenshot} className="card-img-top" alt={object.projectName}/>
-        <div className="card-body">
-            <h5 className="card-title">{object.projectName}</h5>
-            <a href={object.GitHub} className="btn btn-primary">GitHub</a>
-            <a href={object.DeployedLink} className="btn btn-primary">Website</a>
+        <div className="card-body d-flex justify-content-center">
+            <a href={object.GitHub} className="btn btn-primary mt-auto">GitHub</a>
+            <a href={object.DeployedLink} className="btn btn-primary mt-auto">Website</a>
         </div>
     </div>    
     )
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,9 +1,10 @@
 import projects from "../../projects.json";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import './Projects.css';
 
 function Projects(){
     return (
-        <div>
+        <div className="row d-flex justify-content-center">
             <h3 className="projectTitle">Check out my projects:</h3>
             {projects.map((project) => (
                 <ProjectCard 
