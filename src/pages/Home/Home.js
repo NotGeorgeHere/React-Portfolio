4c462d0c8675c1232bf818d600f826ddb14118bc
unknown <ryder.george20@gmail.com>
2023-04-11 12:28:30 +0100

    updated home-page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,15 +7,18 @@ function Home(){
             <div className="jumbotron jumbotron-fluid">
                 <div className="container">
                     <h1 className="display-4">George Ryder - React Portfolio</h1>
-                    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu hendrerit urna. Donec commodo luctus vulputate.</p>
+                    <p className="lead">Front-end website developer, check out my Portfolio!</p>
                     <img src={avatar} alt="Portfolio avatar" className='avatar'></img>
                 </div>
             </div>
             <div className='aboutSection'>
                 <h3 className='AboutMe'>About Me</h3>
-                <p className='aboutMeText'>Sed vel viverra ex, a mattis felis. Fusce faucibus diam ut mauris iaculis, ac efficitur turpis sollicitudin. Nullam sit amet massa et felis dapibus facilisis vel non metus. Fusce risus velit, luctus at suscipit et, scelerisque in augue. Donec fermentum turpis quam, sit amet accumsan metus accumsan in. Praesent ultricies magna nec est pretium lacinia sit amet eget nisi. Etiam id lacus ante. Suspendisse sed fringilla quam. Sed suscipit efficitur lacinia. In sagittis velit ut neque dapibus mollis. Sed elementum dui vel risus placerat consectetur. Aliquam bibendum scelerisque porta. Vivamus pretium augue vel sapien efficitur, eu suscipit velit laoreet.</p>
+                <p className='aboutMeText'>In 2021 I graduated from the University of East Anglia (UEA) with a degree in History and achieved a 2:1. Since then, I have worked for Norfolk County Council and have recently finished a front-end web development course through the UK government's Skills for Life Program. Therefore, I am looking for an appropriate role that suits my skills.
+                </p>
+                <p className='aboutMeText'>Throughout previous employment, volunteering, and my degree I have developed key skills to help me excel under a variety of situations. I have shown an aptitude to work well under pressure, highlighted by my ability to perform well during busy retail periods. Additionally, I am a very social person and developed good working relationships whilst also developing excellent communication skills with customers and key stakeholders. I am looking for a challenging, yet rewarding, work environment with lots to do. I am also looking for a workplace in which I have the ability to thrive and develop personally with the skills I have acquired through previous employment, my degree and the Skills for Life Front-end web development course.</p>
 
-                <p className='aboutMeText'>Nunc a justo sit amet magna varius maximus non sed nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla porttitor, ligula ut fringilla volutpat, eros nibh mattis neque, commodo viverra tellus risus in est. Quisque non feugiat augue. Maecenas a sodales dui. Donec non erat laoreet, eleifend felis et, consectetur eros. Sed et metus lacinia, aliquam mi at, bibendum diam. Nullam vitae pellentesque turpis. Proin mollis, nunc vel pellentesque dictum, mauris lectus vehicula lacus, sit amet viverra quam sapien elementum enim.</p>
+                <h5>Feel free to contact me!</h5>
+                <br></br>
             </div>
 
             <div className="skillsSection">
